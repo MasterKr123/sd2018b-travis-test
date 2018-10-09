6696b793217c65adee0e802b58c2c7bc8d0a6e11
juliand7gj <jorgeeliecercastao@outlook.com>
2018-10-08 19:17:50 -0500

    Update

--- gm_analytics/handlers.py
+++ gm_analytics/handlers.py
@@ -1,6 +1,17 @@
+import logging
+
 def get_user_info(username=None):
+    logging.info('exec get_user_info')
+    logging.debug('username=%s',username)
     return {"user_info": username}
 
 def get_commits_info(username=None, time_range=None):
+    logging.info('exec get_commits_info')
+    logging.debug('username=%s',username)
+    logging.debug('time_range=%s',time_range)
     return [{'commits_count': 10, 'yyyymmdd_date': '20180101'},
             {'commits_count': 10, 'yyyymmdd_date': '20180102'}]
+
+def get_health():
+    logging.info('service health')
+    return {"message":"OK"}
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -3,7 +3,6 @@ import mock
 import connexion
 from gm_analytics import handlers
 
-
 @pytest.fixture(scope='module')
 def client():
     flask_app = connexion.FlaskApp(__name__)
@@ -16,12 +15,16 @@ def user_info():
     return {'user_info': 'some_info'}
 
 
-def test_get_health(client):
-    # GIVEN ...
-    # WHEN I access to the url ...
-    # THEN ...
-    response = client.get('/health')
-    assert response.status_code == 200
+#def test_get_health(client):
+#    # GIVEN ...
+#    # WHEN I access to the url ...
+#    # THEN ...
+#    response = client.get('/health')
+#    assert response.status_code == 200
+
+def test_get_health():
+    message = handlers.get_health()
+    assert {"message":"OK"} == message
 
 
 def test_get_user_info(mocker, user_info):
